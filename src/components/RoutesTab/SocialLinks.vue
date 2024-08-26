0dec158a7204ec417494c1933934a8c9a4706fe0
kifran72 <cr91700kifran@gmail.com>
2024-08-26 13:02:01 +0200

    Clean

--- src/components/RoutesTab/SocialLinks.vue
+++ src/components/RoutesTab/SocialLinks.vue
@@ -7,7 +7,7 @@
     text-color="black"
     color="secondary"
     :icon="feed.icon"
-    :class="!$q.screen.lt.md ? 'q-ml-md ' : ''"
+    :class="!$q.screen.lt.md ? 'q-mr-md' : ''"
     :size="!$q.screen.lt.md ? 'sm' : 'md'"
   />
 </template>
